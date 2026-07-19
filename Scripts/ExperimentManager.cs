88da6feb90279c2a9d9610aaccfedeb24c821c9a
Gaurav <gauravn2211@gmail.com>
2026-07-19 13:41:02 +0100

    various tweaks

--- Scripts/ExperimentManager.cs
+++ Scripts/ExperimentManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -17,9 +18,23 @@ public class ExperimentManager : MonoBehaviour
     public Transform botTransform;
     public Vector3 botSpawnPosition = new Vector3(0.1f, 0.06999993f, -100.5f);
     public Quaternion botSpawnRotation;
+    public float idleYOffset = 1f;
 
     [Header("Haptics")]
     public TactSuitController tactSuitController;
+
+    [Header("Countdown")]
+    public float countdownDuration = 5f;
+    public AudioSource countdownAudioSource;
+    public AudioClip countdownClip;
+
+    [Header("Playback")]
+    public Animator conditionAnimator;
+    public PathWalker pathWalker;
+    public AudioSource footstepAudioSource;
+
+    private Coroutine countdownRoutine;
+    private bool isTransitioning;
 
     void Awake()
     {
@@ -27,19 +42,64 @@ public class ExperimentManager : MonoBehaviour
 
         if (botTransform != null)
             botSpawnRotation = botTransform.rotation;
+    }
 
-        SetCondition(ConditionType.A_VisualAudio1PP);
+    void Start()
+    {
+        RequestCondition(ConditionType.A_VisualAudio1PP);
     }
 
     void Update()
     {
-        if (Keyboard.current.digit1Key.wasPressedThisFrame) SetCondition(ConditionType.A_VisualAudio1PP);
-        if (Keyboard.current.digit2Key.wasPressedThisFrame) SetCondition(ConditionType.B_Combined1PP);
-        if (Keyboard.current.digit3Key.wasPressedThisFrame) SetCondition(ConditionType.C_VisualAudio3PP);
-        if (Keyboard.current.digit4Key.wasPressedThisFrame) SetCondition(ConditionType.D_Control);
+        if (isTransitioning) return;
+
+        if (Keyboard.current.digit1Key.wasPressedThisFrame) RequestCondition(ConditionType.A_VisualAudio1PP);
+        if (Keyboard.current.digit2Key.wasPressedThisFrame) RequestCondition(ConditionType.B_Combined1PP);
+        if (Keyboard.current.digit3Key.wasPressedThisFrame) RequestCondition(ConditionType.C_VisualAudio3PP);
+        if (Keyboard.current.digit4Key.wasPressedThisFrame) RequestCondition(ConditionType.D_Control);
     }
 
-    public void SetCondition(ConditionType condition)
+    public void RequestCondition(ConditionType condition)
+    {
+        if (countdownRoutine != null)
+            StopCoroutine(countdownRoutine);
+
+        FirstPerson = GetFirstPersonForCondition(condition);
+
+        countdownRoutine = StartCoroutine(CountdownThenSetCondition(condition));
+    }
+
+    bool GetFirstPersonForCondition(ConditionType condition)
+    {
+        switch (condition)
+        {
+            case ConditionType.A_VisualAudio1PP: return true;
+            case ConditionType.B_Combined1PP: return true;
+            case ConditionType.C_VisualAudio3PP: return false;
+            case ConditionType.D_Control: return true;
+        }
+        return true;
+    }
+
+    IEnumerator CountdownThenSetCondition(ConditionType condition)
+    {
+        isTransitioning = true;
+
+        StopPlaybackAndReset();
+        ResetBot();
+
+        if (countdownAudioSource != null && countdownClip != null)
+            countdownAudioSource.PlayOneShot(countdownClip);
+
+        yield return new WaitForSeconds(countdownDuration);
+
+        ApplyCondition(condition);
+        StartPlayback();
+
+        isTransitioning = false;
+    }
+
+    void ApplyCondition(ConditionType condition)
     {
         CurrentCondition = condition;
 
@@ -70,9 +130,41 @@ public class ExperimentManager : MonoBehaviour
         if (tactSuitController != null)
             tactSuitController.SetHapticsActive(HapticsEnabled);
 
-        ResetBot();
-
         Debug.Log($"Condition set: {condition} | Haptics:{HapticsEnabled} Audio:{AudioEnabled} 1PP:{FirstPerson}");
+    }
+
+    void StopPlaybackAndReset()
+    {
+        if (conditionAnimator != null)
+            conditionAnimator.Play("Idle", 0, 0f);
+
+        if (pathWalker != null)
+        {
+            pathWalker.IsPaused = true;
+            pathWalker.ResetPosition();
+        }
+
+        if (footstepAudioSource != null)
+            footstepAudioSource.Stop();
+
+        if (countdownAudioSource != null)
+            countdownAudioSource.Stop();
+
+        if (botTransform != null)
+        {
+            Vector3 pos = botTransform.position;
+            pos.y = botSpawnPosition.y + idleYOffset;
+            botTransform.position = pos;
+        }
+    }
+
+    void StartPlayback()
+    {
+        if (conditionAnimator != null)
+            conditionAnimator.Play("mixamo_com", 0, 0f);
+
+        if (pathWalker != null)
+            pathWalker.IsPaused = false;
     }
 
     void ResetBot()
--- Scripts/PathWalker.cs
+++ Scripts/PathWalker.cs
@@ -5,6 +5,8 @@ public class PathWalker : MonoBehaviour
     public float walkSpeed = 1.4f;
     public float resetDistance = 23f;
 
+    public bool IsPaused = true; 
+
     private Vector3 startPosition;
 
     void Start()
@@ -14,11 +16,18 @@ public class PathWalker : MonoBehaviour
 
     void Update()
     {
+        if (IsPaused) return;
+
         transform.Translate(Vector3.forward * walkSpeed * Time.deltaTime);
 
         if (transform.position.z - startPosition.z >= resetDistance)
         {
             transform.position = startPosition;
         }
+    }
+
+    public void ResetPosition()
+    {
+        transform.position = startPosition;
     }
 }
